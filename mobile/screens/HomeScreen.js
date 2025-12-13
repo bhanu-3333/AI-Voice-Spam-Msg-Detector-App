81dcf73107dda4cb9401f9b97f9497803ff392c2
bhanu-3333 <bhanuu339@gmail.com>
2025-12-13 23:13:07 +0530

    updated home screen

---
+++ mobile/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🛡️ AI Scam Detector</Text>
+      <Button title="Open Spam Checker" onPress={() => navigation.navigate("Checker")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#020617"
+  },
+  title: {
+    color: "white", fontSize: 26, marginBottom: 20
+  }
+});

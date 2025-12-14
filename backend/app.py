c3628968b0dcab68254ff87992c05b7087950b2c
bhanu-3333 <bhanuu339@gmail.com>
2025-12-14 10:41:25 +0530

    model phising changed

--- backend/app.py
+++ backend/app.py
@@ -7,18 +7,17 @@ app = Flask(__name__)
 # ✅ ENABLE CORS (IMPORTANT)
 CORS(app)
 
-
 classifier = pipeline(
     "text-classification",
     model="cybersectony/phishing-email-detection-distilbert_v2.1"
 )
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
     text = data.get("text")
     result = classifier(text)
     return jsonify(result)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)

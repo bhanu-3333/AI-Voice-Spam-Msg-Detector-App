1c0902c1204fd42d68920e0a94398b7e1f6c24bc
bhanu-3333 <bhanuu339@gmail.com>
2025-12-14 10:40:08 +0530

    predict route modified

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # ✅ ENABLE CORS (IMPORTANT)
 CORS(app)
 
+
 classifier = pipeline(
     "text-classification",
     model="cybersectony/phishing-email-detection-distilbert_v2.1"
@@ -17,6 +18,7 @@ def predict():
     text = data.get("text")
     result = classifier(text)
     return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)

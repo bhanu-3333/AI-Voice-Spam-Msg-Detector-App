5fc17dfbe701ad07eb236910d15296bd7657c76c
bhanu-3333 <bhanuu339@gmail.com>
2026-01-09 20:35:47 +0530

    email scam predicted is successfully executed

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -8,6 +8,7 @@ export default function HomeScreen({ navigation }) {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#020617"
